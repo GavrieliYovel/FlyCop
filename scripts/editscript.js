67c72b1f505970d2eb8ff701296280ff74ffc3c7
peerfikhman <peerfikhman@gmail.com>
2022-07-11 15:25:54 +0300

    ajax

--- scripts/editscript.js
+++ scripts/editscript.js
@@ -16,7 +16,6 @@ function resetFormBtn() {
 
     document.getElementById("resetBtn").onclick = () => {
         for (let i = 0; i < outputs.length; i++) {
-            console.log(ranges[i]);
             outputs[i].value = ranges[i].min;
             ranges[i].value = ranges[i].min;
         }
@@ -27,16 +26,51 @@ function resetFormBtn() {
 
 $(document).ready(function () {
     $("#standStill").click(function () {
-        console.log($(this).val());
         $("#maxDistance").attr("disabled", "disabled");
     });
 
     $("#patrol").click(function () {
-        console.log($(this).val());
         $("#maxDistance").removeAttr("disabled");
     });
 });
 
+
+
+function showMissionSC(missionSC) {
+
+    let scDiv = document.getElementById("missSC");
+
+    for (const sc of missionSC.shortcuts) {
+        let btn = document.createElement('button');
+        btn.className = "btn btn-secondary";
+        btn.innerHTML = sc["missionName"];
+        let mission = document.querySelectorAll('input[type=radio]')
+        btn.onclick = () => {
+
+            if (mission[0].value == sc["type"]) {
+                mission[0].checked = 'cheked';
+                ranges[2].disabled = false;
+
+            }
+            else {
+                mission[1].checked = 'cheked';
+                ranges[2].disabled = true;
+            }
+            ranges[0].value = outputs[0].value = sc["duration"];
+            ranges[1].value = outputs[1].value = sc["maxAltitude"];
+            ranges[2].value = outputs[2].value = sc["maxDistance"];
+
+        }
+        scDiv.appendChild(btn);
+    }
+
+
+}
+
+
+fetch("json/missonshortcuts.json")
+    .then(response => response.json())
+    .then(data => showMissionSC(data));
 
 
 
